a8834d6b4db35415e7cf36c3cf5e77d41f628981
eldendo <marc.dendooven@gmail.com>
2021-12-15 13:06:59 +0100

    Add files via upload

--- test.pas
+++ test.pas
@@ -24,18 +24,18 @@ SOFTWARE.
 
 program test;
 uses    cmem, // needed to use the c procedure free() 
-			  // to deallocate buf
+              // to deallocate buf
         fpc_readline;
 
 var buf:Pchar;
 
 begin
-	while true do begin
-		buf := readline('>> ');
-		if buf = nil then exit;
-		if (length(buf) > 0) then add_history(buf);
-		writeln('[',buf,']');
+    while true do begin
+        buf := readline('>> ');
+        if buf = nil then exit;
+        if (length(buf) > 0) then add_history(buf);
+        writeln('[',buf,']');
         free(buf)    // readline malloc's a new buffer every time.
-					 // pascals memFree() gives runtime errors.
-	end
+                     // pascals memFree() gives runtime errors.
+    end
 end.
